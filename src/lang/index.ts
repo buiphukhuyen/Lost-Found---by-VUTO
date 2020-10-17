59022000841dc6a06f5526e74439283fa276268c
hoai06032000 <73026619+hoai06032000@users.noreply.github.com>
2020-10-17 21:26:06 +0700

    Sửa thông báo BOT

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,14 +1,17 @@
+//Để sửa thông báo, thì thay đổi nội dung trong dấu "". Ví dụ MAINTENANCE: "[🤖] Server hiện đang bảo trì..." => MAINTENANCE: "[🤖] Đang bảo trì rồi! Bạn vui lòng đợi thông báo nhé!"
+//Để phân biệt giữa bot và tin nhắn của người chat thì thêm keyword [BOT] trước thông báo đó! Ở đây do anh dùng ICON mà bên máy em không thấy. Giờ anh đổi lại thành BOT
 export default {
-  MAINTENANCE: "[🤖] Server hiện đang bảo trì...",
-
+  //Được Edit (Thông báo khi bảo trì)
+  MAINTENANCE: "[BOT] Server hiện đang bảo trì...",
+  //Được Edit (Thông báo mở đầu)
   FIRST_COME:
-    "[🤖] Chào mừng bạn đến với Lost Found - By Vuto. Trước khi bắt đầu, hãy chắc chắn rằng bạn đã chọn đúng giới tính người muốn chat cùng.\n\nẤn trợ giúp (hoặc gửi Trợ giúp) để xem thêm.",
-
+    "[BOT] Chào mừng bạn đến với Lost Found - By Vuto. Trước khi bắt đầu, hãy chắc chắn rằng bạn đã chọn đúng giới tính người muốn chat cùng.\n\nẤn trợ giúp (hoặc gửi Trợ giúp) để xem thêm.",
+  //Được Edit (Thông báo đầu tiên sau mở đầu)
   INSTRUCTION:
-    '[🤖] Gửi "Bắt đầu chat" hoặc bấm vào nút để Tìm bạn Chat ngẫu nhiên.',
-
+    '[BOT] Gửi "À nhon xê dô, hello các bạn bắt đầu chat nha',
+  //Được Edit (Thông báo khi ấn vào nút "Trợ giúp")
   HELP_TXT:
-    "[🤖] Danh sách các lệnh:\n" +
+    "[BOT] Danh sách các lệnh:\n" +
     "- Bắt đầu chat: Bắt đầu tìm bạn chat\n" +
     "- Kết thúc: Kết thúc chat\n" +
     "- Trợ giúp: Xem trợ giúp\n" +
@@ -19,38 +22,56 @@ export default {
     "- Nam - Nam: Nam tìm Nam chat cùng\n" +
     "- Nữ - Nữ: Nữ tìm Nữ chat cùng",
 
+  //Được Edit (Thông báo khi ấn vào nút "Tìm" (Nam, Nữ, Ngẫu nhiên))
   START_OKAY:
-    "[🤖] Đợi xíu nhé! Hệ thống sẽ thông báo ngay cho bạn khi tìm được 💓",
+    "[BOT] Đợi xíu nhé! Hệ thống sẽ thông báo ngay cho bạn khi tìm được 💓",
+  
+  //Được Edit (TB trường hợp chat ngẫu nhiên - không chọn giới tính)
   START_WARN_GENDER:
-    "[🤖] Lưu ý: Bạn không chọn Giới tính. Có thể bạn sẽ phải đợi lâu hơn.",
+    "[BOT] Lưu ý: Bạn không chọn Giới tính. Có thể bạn sẽ phải đợi lâu hơn.",
+  
+  //Được Edit (TB khi đã vào trò chuyện mà ấn vào nút "Tìm" - chỉ khi nào kết thúc mới tìm mới được_
   START_ERR_ALREADY:
-    "[🤖] Bạn không thể Bắt đầu Cuộc trò chuyện mới khi chưa Kết thúc...",
+    "[BOT] Bạn không thể Bắt đầu Cuộc trò chuyện mới khi chưa Kết thúc...",
 
+  //Được Edit (TB trường hợp đang Tìm bạn chat)
   WAITING:
-    '[🤖] Đang tìm bạn Chat... Nếu bạn muốn đổi Giới tính, gửi "Kết thúc" sau đó Chọn Giới tính mới.',
-  CONNECTED: "[🤖] Ping Ping! Đã tìm thấy bạn rồi! Gửi lời chào với nhau nào!",
+    '[BOT] Đang tìm bạn Chat... Nếu bạn muốn đổi Giới tính, gửi "Kết thúc" sau đó Chọn Giới tính mới.',
+  
+  //Được Edit (TB khi đã tìm thấy bạn chat)
+  CONNECTED: "[BOT] Ping Ping! Đã tìm thấy bạn rồi! Gửi lời chào với nhau nào!",
 
+  //Được Edit (TB khi kết thúc chat)
   END_CHAT:
-    '[🤖] Kết thúc Chat!\nGửi "Bắt đầu chat" hoặc Chọn nút bên dưới để tìm bạn chat. \n[Bổ sung]Hãy Gửi yêu cầu trong trường hợp phát hiện lỗi hay bị quấy rối',
+    '[BOT] Kết thúc Chat!\nGửi "Bắt đầu chat" hoặc Chọn nút bên dưới để tìm bạn chat. \n[Bổ sung]Hãy Gửi yêu cầu trong trường hợp phát hiện lỗi hay bị quấy rối',
+  
+  //Được Edit (TB khi kết thúc chat - ở phía bên được kết thúc)
   END_CHAT_PARTNER:
-    '[🤖] Hic! Bạn ý đã ngắt kết nối rồi \nGửi "Bắt đầu chat" hoặc bấm vào nút để Tìm bạn Chat mới nha!',
+    '[BOT] Hic! Bạn ý đã ngắt kết nối rồi \nGửi "Bắt đầu chat" hoặc bấm vào nút để Tìm bạn Chat mới nha!',
+  
+  //Được Edit (TB khi không tìm ra bạn chat - khi 15 phút)
   END_CHAT_FORCE:
-    "[🤖] Hiện tại không tìm thấy được bạn chat phù hợp. Bạn hãy thử lại nhé!",
+    "[BOT] Hiện tại không tìm thấy được bạn chat phù hợp. Bạn hãy thử lại nhé!",
+  
+  //Được Edit (TB khi gửi 1 số file k hỗ trợ - chỉ hình ảnh, âm thanh, tin nhắn, icon là hỗ trợ thôi)
+  ERR_ATTACHMENT: "[BOT] Lỗi: Chatbot chưa hỗ trợ gửi dạng dữ liệu này!",
+  ATTACHMENT_LINK: "[BOT] Bạn ý đã gửi 1 đường Link: ",
 
-  ERR_ATTACHMENT: "[🤖] Lỗi: Chatbot chưa hỗ trợ gửi dạng dữ liệu này!",
-  ATTACHMENT_LINK: "[🤖] Bạn ý đã gửi 1 đường Link: ",
-
+  //Được Edit (TB thêm)
   GENDER_ERR:
-    "[🤖] Lỗi: Giới tính nhập vào không hợp lệ!\n\nẤn trợ giúp (hoặc gửi Trợ giúp) để xem thêm.",
-  GENDER_WRITE_OK: "[🤖] Bạn đã chọn Giới tính mong muốn tìm được là: ",
+    "[BOT] Lỗi: Giới tính nhập vào không hợp lệ!\n\nẤn trợ giúp (hoặc gửi Trợ giúp) để xem thêm.",
+  GENDER_WRITE_OK: "[BOT] Bạn đã chọn Giới tính mong muốn tìm được là: ",
   GENDER_WRITE_WARN:
     "\n[Thông tin thêm] Like Page và Join Group để xem các tính năng mới nha!",
+  
+  //Phần này TUYỆT ĐỐI không được Edit (Vì đây là Keyword)
   GENDER_ARR_UNKNOWN: "Cả hai",
   GENDER_ARR_MALE: "Nam-Nữ",
   GENDER_ARR_FEMALE: "Nữ-Nam",
   GENDER_ARR_GAY: "Nam-Nam",
   GENDER_ARR_LES: "Nữ-Nữ",
-
+  
+  //Phần này cũng TUYỆT ĐỐI không được Edit
   KEYWORD_START: "batdau",
   KEYWORD_END: "ketthuc",
   KEYWORD_GENDER: "tim",
@@ -66,16 +87,17 @@ export default {
   KEYWORD_GENDER_LES: "les",
   KEYWORD_HELP: "trogiup",
 
-  ERR_FAKE_MSG: "[🤖] Lỗi: Bạn không được giả mạo tin nhắn của BOT!",
-  ERR_DATABASE: "[🤖] Lỗi: Không thể kết nối với Database. Hãy báo cho Admin!",
+  //Được Edit
+  ERR_FAKE_MSG: "[BOT] Lỗi: Bạn không được giả mạo tin nhắn của BOT!",
+  ERR_DATABASE: "[BOT] Lỗi: Không thể kết nối với Database. Hãy báo cho Admin!",
   ERR_TOO_LONG:
-    "[🤖] Lỗi: Tin nhắn quá dài (nhiều hơn 2000 ký tự). Hãy chia nhỏ tin nhắn và gửi dần.",
+    "[BOT] Lỗi: Tin nhắn quá dài (nhiều hơn 2000 ký tự). Hãy chia nhỏ tin nhắn và gửi dần.",
   ERR_200:
-    "[🤖] Bạn Chat không thể nhận tin nhắn do đã xóa Inbox hoặc Block page.",
+    "[BOT] Bạn Chat không thể nhận tin nhắn do đã xóa Inbox hoặc Block page.",
   ERR_10:
-    '[🤖] Bạn Chat không thể nhận tin nhắn do 2 bạn không nói chuyện trong 24h. Gửi "Kết thúc" để kết thúc chat.',
+    '[BOT] Bạn Chat không thể nhận tin nhắn do 2 bạn không nói chuyện trong 24h. Gửi "Kết thúc" để kết thúc chat.',
   ERR_SERVER:
-    "[🤖] Có lỗi xảy ra với Chatbot. Tin nhắn của bạn chưa được xử lý. Bạn hãy thử lại sau 1 phút nữa nhé.",
+    "[BOT] Có lỗi xảy ra với Chatbot. Tin nhắn của bạn chưa được xử lý. Bạn hãy thử lại sau 1 phút nữa nhé.",
   ERR_UNKNOWN:
-    '[🤖] Server xảy ra lỗi nhưng không nghiêm trọng lắm\nHãy gửi "Kết thúc" để thoát ra và thử lại',
+    '[BOT] Server xảy ra lỗi nhưng không nghiêm trọng lắm\nHãy gửi "Kết thúc" để thoát ra và thử lại',
 };
